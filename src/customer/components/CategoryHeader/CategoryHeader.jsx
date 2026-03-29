10d106a4d7b7627487fcf45eb17c3992488de488
Simran Kadam <simrankadamkb12@gmail.com>
2026-03-29 13:53:57 +0530

    changes

--- src/customer/components/CategoryHeader/CategoryHeader.jsx
+++ src/customer/components/CategoryHeader/CategoryHeader.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const CategoryHeader = ({ title, showExploreButton = true }) => {
+  console.log('🎯 CategoryHeader rendering:', { title, showExploreButton })
+  
   return (
     <div className="relative mb-8 lg:mb-12 flex items-center justify-between">
       <div>
